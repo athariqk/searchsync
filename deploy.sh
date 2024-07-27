7c2b1db0a9fb19eb7ca4268cc872868b5f1ac0dd
athariqk <alib.athariq@gmail.com>
2024-07-27 13:06:39 +0700

    ops(build.yml): trying to fix exec perm again (2)

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,9 @@ TO_PATH="${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}"
 
 echo "Deplying from " ${FROM_PATH} " to " ${REMOTE_PATH}
 
+# Give everything execute flag
+chmod -R +x ${FROM_PATH}
+
 # Ensure the remote path exists
 ssh "${REMOTE_USER}@${REMOTE_HOST}" "mkdir -p ${REMOTE_PATH}"
 

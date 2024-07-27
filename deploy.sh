6d862fad456b6e04f7fbf845251a650430554967
athariqk <alib.athariq@gmail.com>
2024-07-27 12:15:28 +0700

    ops: fix build.yml

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,6 @@ TO_PATH="${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}"
 ssh "${REMOTE_USER}@${REMOTE_HOST}" "mkdir -p ${REMOTE_PATH}"
 
 # Sync the build artifacts
-rsync -avzhe ssh --delete "$FROM_PATH" "$TO_PATH"
+rsync -avzhe ssh "$FROM_PATH" "$TO_PATH"
 
 echo "Deployment complete."

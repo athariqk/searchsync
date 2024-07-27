73c4dea9605e7f0e0ce991d7f80315be4e839b1e
athariqk <alib.athariq@gmail.com>
2024-07-27 12:23:16 +0700

    ops(deploy.sh): print paths

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,8 @@ echo "Starting deployment..."
 FROM_PATH="build_output/"
 TO_PATH="${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}"
 
+echo "Deplying from " ${FROM_PATH} " to " ${REMOTE_PATH}
+
 # Ensure the remote path exists
 ssh "${REMOTE_USER}@${REMOTE_HOST}" "mkdir -p ${REMOTE_PATH}"
 
